80b38c91b6e6c9a9098bf813b6c035dfbf585a20
Mark <engineermarkpro@gmail.com>
2024-07-19 22:13:29 +0200

    ver

--- inspector.user.js
+++ inspector.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         osu! scores inspector
 // @namespace    https://score.kirino.sh
-// @version      2024-07-16.30
+// @version      2024-07-19.31
 // @description  Display osu!alt and scores inspector data on osu! website
 // @author       Amayakase
 // @match        https://osu.ppy.sh/*
