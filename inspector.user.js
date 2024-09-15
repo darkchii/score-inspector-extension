8f78b7fd2e2cfa0716d54efb7db49a74963ad29c
Mark <engineermarkpro@gmail.com>
2024-09-16 00:26:15 +0200

    validate check beatmap(set) id

--- inspector.user.js
+++ inspector.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         osu! scores inspector
 // @namespace    https://score.kirino.sh
-// @version      2024-09-16.47
+// @version      2024-09-16.48
 // @description  Display osu!alt and scores inspector data on osu! website
 // @author       Amayakase
 // @match        https://osu.ppy.sh/*
